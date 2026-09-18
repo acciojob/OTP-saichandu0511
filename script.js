a2dd5623eb9837ec4d9009db244c4ed3cb0b8e34
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-18 21:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+
+    code.addEventListener("input", (e) => {
+
+        
+        code.value = code.value.replace(/\D/g, "");
+
+        
+        if (code.value !== "" && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+
+        if (e.key === "Backspace") {
+
+            
+            if (code.value !== "") {
+                code.value = "";
+            }
+            
+            else if (index > 0) {
+                codes[index - 1].focus();
+                codes[index - 1].value = "";
+            }
+        }
+    });
+});
